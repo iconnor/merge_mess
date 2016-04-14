f69f62a754a0e08ade8e49abe55da1f3775926d3
Ian Connor <iconnor@projectlounge.com>
2016-04-14 12:56:39 +1000

    Cleanup commit

--- violation.rb
+++ violation.rb
@@ -1,6 +1,4 @@
-#simple ruby file with violations
-puts "string wiht double quotes"
+# simple ruby file with violations
+puts 'string wiht double quotes'
 
-if true
-  puts 'guard claused should be used'
-end
+puts 'guard claused should be used'
